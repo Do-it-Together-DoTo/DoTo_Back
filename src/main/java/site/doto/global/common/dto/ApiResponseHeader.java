f5c366dd2e372ceef4261ecb73dbb7a098c69031
eyxnz <todaytubuck@naver.com>
2024-05-06 18:53:59 +0900

    feat : ResponseDto 함수에 Enum 반영

--- src/main/java/site/doto/global/common/dto/ApiResponseHeader.java
+++ src/main/java/site/doto/global/common/dto/ApiResponseHeader.java
@@ -1,11 +1,21 @@
 package site.doto.global.common.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import site.doto.global.common.code.ErrorCode;
+import site.doto.global.common.code.SuccessCode;
 
 @Data
-@AllArgsConstructor
 public class ApiResponseHeader {
     private int httpStatusCode;
     private String message;
+
+    public ApiResponseHeader(SuccessCode s) {
+        this.httpStatusCode = s.getHttpStatusCode();
+        this.message = s.getMessage();
+    }
+
+    public ApiResponseHeader(ErrorCode e) {
+        this.httpStatusCode = e.getHttpStatusCode();
+        this.message = e.getMessage();
+    }
 }
--- src/main/java/site/doto/global/common/dto/ResponseDto.java
+++ src/main/java/site/doto/global/common/dto/ResponseDto.java
@@ -2,27 +2,21 @@ package site.doto.global.common.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import site.doto.global.common.code.ErrorCode;
+import site.doto.global.common.code.SuccessCode;
 
 @Data
 @AllArgsConstructor
 public class ResponseDto<T> {
-    private final static int SUCCESS = 200;
-
-    private final static int FAILED = 500;
-
-    private final static String SUCCESS_MESSAGE = "성공적으로 처리되었습니다.";
-
-    private final static String FAILED_MESSAGE = "서버 내부 오류가 발생했습니다.";
-
     private ApiResponseHeader header;
 
     private T body;
 
-    public static <T> ResponseDto<T> success(T body) {
-        return new ResponseDto<>(new ApiResponseHeader(SUCCESS, SUCCESS_MESSAGE), body);
+    public static <T> ResponseDto<T> success(SuccessCode s, T body) {
+        return new ResponseDto<>(new ApiResponseHeader(s), body);
     }
 
-    public static <T> ResponseDto<T> fail(T body) {
-        return new ResponseDto<>(new ApiResponseHeader(FAILED, FAILED_MESSAGE), null);
+    public static <T> ResponseDto<T> fail(ErrorCode e) {
+        return new ResponseDto<>(new ApiResponseHeader(e), null);
     }
 }
