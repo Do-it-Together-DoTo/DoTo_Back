67464ac01a84d18281cf5b989e7ad6bc3049c715
류인환 <mmzz0219@gmail.com>
2024-05-08 22:16:17 +0900

    feat : p6spy 적용

--- src/main/java/site/doto/global/config/AppConfig.java
+++ src/main/java/site/doto/global/config/AppConfig.java
@@ -1,21 +1,29 @@
 package site.doto.global.config;
 
+import com.p6spy.engine.spy.P6SpyOptions;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import site.doto.global.logtracer.LogTrace;
 import site.doto.global.logtracer.LogTraceAspect;
+import site.doto.global.p6spy.P6spySqlFormatConfiguration;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 @RequiredArgsConstructor
 public class AppConfig {
-
     private final LogTrace logTrace;
 
     @Bean
     @ConditionalOnProperty(value = "logTracer", havingValue = "true")
     public LogTraceAspect logTraceAspect() {
         return new LogTraceAspect(logTrace);
+    }
+
+    @PostConstruct
+    public void setLogMessageFormat() {
+        P6SpyOptions.getActiveInstance().setLogMessageFormat(P6spySqlFormatConfiguration.class.getName());
     }
 }
