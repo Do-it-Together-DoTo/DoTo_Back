80bd3cc106ed1efff2579f1d45db8c7f2bea9e3c
Cutaku <107826896+Cutaku@users.noreply.github.com>
2024-05-16 10:29:15 +0900

    fix : 정적 리소스를 불러오지 못하는 문제 해결 (#28)

--- src/main/java/site/doto/global/handler/GlobalExceptionHandler.java
+++ src/main/java/site/doto/global/handler/GlobalExceptionHandler.java
@@ -3,13 +3,11 @@ package site.doto.global.handler;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import site.doto.global.dto.ResponseDto;
 import site.doto.global.exception.CustomException;
 import site.doto.global.status_code.ErrorCode;
 
 @RestControllerAdvice
-@EnableWebMvc
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     protected ResponseDto<?> handleException(Exception e) {
