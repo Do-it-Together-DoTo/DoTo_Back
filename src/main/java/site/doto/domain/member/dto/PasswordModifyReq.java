e37bb5d56cc155b536c0271ca88d4194afdb58fc
jeongjiyeon315 <jung_j_yeon@naver.com>
2024-05-19 16:34:08 +0900

    feat : 비밀번호 변경 Request Dto NotNull 어노테이션 추가

--- src/main/java/site/doto/domain/member/dto/PasswordModifyReq.java
+++ src/main/java/site/doto/domain/member/dto/PasswordModifyReq.java
@@ -1,11 +1,15 @@
 package site.doto.domain.member.dto;
 
 import lombok.Data;
+경
+import javax.validation.constraints.NotNull;
 
 @Data
 public class PasswordModifyReq {
+    @NotNull
     private String currentPassword;
 
+    @NotNull
     private String changePassword;
 
 }
