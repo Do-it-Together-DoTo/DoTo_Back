28a56e7a728eb91a887cb079b1313cba3852374e
jeongjiyeon315 <jung_j_yeon@naver.com>
2024-05-19 16:35:30 +0900

    feat : 비밀번호 찾기 Request Dto NotNull 어노테이션 추가

--- src/main/java/site/doto/domain/member/dto/PasswordFindReq.java
+++ src/main/java/site/doto/domain/member/dto/PasswordFindReq.java
@@ -2,12 +2,17 @@ package site.doto.domain.member.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class PasswordFindReq {
+    @NotNull
     private String email;
 
+    @NotNull
     private String authCode;
 
+    @NotNull
     private String changePassword;
 
 }
