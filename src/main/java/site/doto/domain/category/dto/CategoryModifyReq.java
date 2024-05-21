2970efda14ebea30e1eccb298ee0941c1ab34463
류인환 <mmzz0219@gmail.com>
2024-05-21 20:59:26 +0900

    feat : 불필요한 메소드 삭제

--- src/main/java/site/doto/domain/category/dto/CategoryModifyReq.java
+++ src/main/java/site/doto/domain/category/dto/CategoryModifyReq.java
@@ -1,9 +1,6 @@
 package site.doto.domain.category.dto;
 
 import lombok.Data;
-import site.doto.domain.category.entity.Category;
-import site.doto.domain.category.enums.Color;
-import site.doto.domain.member.entity.Member;
 
 @Data
 public class CategoryModifyReq {
@@ -14,14 +11,4 @@ public class CategoryModifyReq {
     private Boolean isActivated;
 
     private String color;
-
-    public Category toEntity(Member member) {
-        return Category.builder()
-                .member(member)
-                .contents(contents)
-                .isPublic(isPublic)
-                .isActivated(isActivated)
-                .color(Color.valueOf(color))
-                .build();
-    }
 }
