3e36bd713cf33f1f2aaac05322878e496121ca6b
류인환 <mmzz0219@gmail.com>
2024-06-11 22:05:35 +0900

    chore : 사용하지 않은 import문 삭제

--- src/test/java/site/doto/domain/member/controller/MemberControllerTest.java
+++ src/test/java/site/doto/domain/member/controller/MemberControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.headers.HeaderDocumentation;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
