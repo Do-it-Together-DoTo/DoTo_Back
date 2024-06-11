d5166bbe3aa2cc4860bf038188f48fdf380eda12
eyxnz <todaytubuck@naver.com>
2024-06-11 17:08:56 +0900

    feat : 유저 검색 ModelAttribute로 변경

--- src/test/java/site/doto/domain/member/controller/MemberControllerTest.java
+++ src/test/java/site/doto/domain/member/controller/MemberControllerTest.java
@@ -25,6 +25,7 @@ import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -371,19 +372,16 @@ class MemberControllerTest {
     @DisplayName("유저 검색_성공")
     public void members_search_success() throws Exception {
         // given
-        MembersSearchReq membersSearchReq = new MembersSearchReq();
-        membersSearchReq.setSearchWord("검색어");
-        membersSearchReq.setLastMemberId(10000L);
-
-        String content = gson.toJson(membersSearchReq);
+        String keyword = "검색어";
+        Long lastMemberId = 10000L;
 
         // when
         ResultActions actions = mockMvc.perform(
                 get("/members/search")
                         .header("Authorization", jwtToken)
+                        .param("keyword", keyword)
+                        .param("lastMemberId", String.valueOf(lastMemberId))
                         .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(content)
         );
 
         // then
@@ -401,13 +399,9 @@ class MemberControllerTest {
                                 .requestHeaders(
                                         HeaderDocumentation.headerWithName("Authorization").description("JWT 토큰")
                                 )
-                                .requestFields(
-                                        List.of(
-                                                fieldWithPath("searchWord").type(JsonFieldType.STRING)
-                                                        .description("검색어"),
-                                                fieldWithPath("lastMemberId").type(JsonFieldType.NUMBER)
-                                                        .description("마지막 유저 Id(Optional)").optional()
-                                        )
+                                .requestParameters(
+                                        parameterWithName("keyword").description("검색어"),
+                                        parameterWithName("lastMemberId").description("마지막 유저 Id(Optional)").optional()
                                 )
                                 .responseFields(
                                         List.of(
@@ -435,7 +429,6 @@ class MemberControllerTest {
                                                         .description("요소의 수")
                                         )
                                 )
-                                .requestSchema(Schema.schema("유저 검색 Request"))
                                 .responseSchema(Schema.schema("유저 검색 Response"))
                                 .build()
                         )
