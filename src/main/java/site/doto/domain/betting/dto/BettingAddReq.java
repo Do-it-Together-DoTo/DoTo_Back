f35dca139ff9a913e9ee7e1369bb3613acba3c51
류인환 <mmzz0219@gmail.com>
2024-06-13 18:14:03 +0900

    feat : 베팅 생성 API 구현

--- src/main/java/site/doto/domain/betting/dto/BettingAddReq.java
+++ src/main/java/site/doto/domain/betting/dto/BettingAddReq.java
@@ -1,6 +1,7 @@
 package site.doto.domain.betting.dto;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import site.doto.domain.betting.entity.Betting;
 import site.doto.domain.member.entity.Member;
 import site.doto.domain.todo.entity.Todo;
@@ -13,6 +14,7 @@ public class BettingAddReq {
     Long todoId;
 
     @NotNull
+    @Length(min = 2, max = 20)
     String name;
 
     public Betting toEntity(Member member, Todo todo) {
--- src/main/java/site/doto/domain/betting/repository/BettingRepository.java
+++ src/main/java/site/doto/domain/betting/repository/BettingRepository.java
@@ -1,10 +1,19 @@
 package site.doto.domain.betting.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import site.doto.domain.betting.entity.Betting;
 
 import java.util.Optional;
+import java.util.List;
 
 public interface BettingRepository extends JpaRepository<Betting, Long> {
     Optional<Betting> findById(Long bettingId);
+
+    @Query("select b from Betting b join b.todo t " +
+            "where b.member.id = :memberId and t.date >= current_date")
+    List<Betting> findAfterToday(@Param("memberId") Long memberId, Pageable pageable);
+
 }
