e786fa38332555f9c510d912c8d81d883bba08c3
jeongjiyeon315 <jung_j_yeon@naver.com>
2024-06-15 01:18:22 +0900

    feat : 활성화된 카테고리 개수 반환하는 함수 추가

--- src/main/java/site/doto/domain/category/repository/CategoryRepository.java
+++ src/main/java/site/doto/domain/category/repository/CategoryRepository.java
@@ -1,10 +1,20 @@
 package site.doto.domain.category.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import site.doto.domain.category.entity.Category;
 
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findById(Long categoryId);
+
+    @Query("select count(*) " +
+    "from Category c " +
+    "where c.member.id = :memberId " +
+    "and c.isActivated = true")
+    int countByMemberId(@Param("memberId") Long memberId);
+
 }
