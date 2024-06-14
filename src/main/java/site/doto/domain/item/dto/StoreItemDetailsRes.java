6457328e8addaa385e0d02645ed46b8376a3ff43
eyxnz <todaytubuck@naver.com>
2024-06-15 01:17:07 +0900

    feat : 아이템 개별 조회 응답 Dto에 toDto 메소드 추가

--- src/main/java/site/doto/domain/item/dto/StoreItemDetailsRes.java
+++ src/main/java/site/doto/domain/item/dto/StoreItemDetailsRes.java
@@ -1,15 +1,24 @@
 package site.doto.domain.item.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import site.doto.domain.item.entity.ItemType;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class StoreItemDetailsRes {
     private String img;
 
     private String name;
 
     private String description;
-    
+
+    public static StoreItemDetailsRes toDto(ItemType itemType) {
+        return StoreItemDetailsRes.builder()
+                .img(itemType.getImg())
+                .name(itemType.getName())
+                .description(itemType.getDescription()).build();
+    }
 }
