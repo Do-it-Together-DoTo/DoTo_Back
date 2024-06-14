4be931233110abfd51aa0582cffd6a5af6dee432
eyxnz <todaytubuck@naver.com>
2024-06-14 13:10:04 +0900

    feat : 멤버 디비에 제약조건 추가

--- src/main/java/site/doto/global/handler/GlobalExceptionHandler.java
+++ src/main/java/site/doto/global/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package site.doto.global.handler;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -34,5 +35,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NoHandlerFoundException.class)
     protected ResponseDto<?> handleNoHandlerFoundException(NoHandlerFoundException e) {
         return ResponseDto.fail(ErrorCode.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    protected ResponseDto<?> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        return ResponseDto.fail(ErrorCode.BAD_REQUEST);
     }
 }
