4b9d8e4e3d0ed24a204a472321991cf0d0a927f8
jeongjiyeon315 <jung_j_yeon@naver.com>
2024-06-15 01:17:26 +0900

    feat : contents max length 추가 및 toEntity 함수 파라미터 추가

--- src/main/java/site/doto/domain/category/dto/CategoryAddReq.java
+++ src/main/java/site/doto/domain/category/dto/CategoryAddReq.java
@@ -1,18 +1,15 @@
 package site.doto.domain.category.dto;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.Length;
 import site.doto.domain.category.entity.Category;
 import site.doto.domain.category.enums.Color;
 import site.doto.domain.member.entity.Member;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
 public class CategoryAddReq {
-    @NotBlank
-    @Length(max = 20)
+    @NotNull
     private String contents;
 
     @NotNull
@@ -21,13 +18,12 @@ public class CategoryAddReq {
     @NotNull
     private String color;
 
-    public Category toEntity(Member member, int seq) {
+    public Category toEntity(Member member) {
         return Category.builder()
                 .member(member)
                 .contents(contents)
                 .isPublic(isPublic)
                 .isActivated(true)
-                .seq(seq+1)
                 .color(Color.valueOf(color))
                 .build();
     }
