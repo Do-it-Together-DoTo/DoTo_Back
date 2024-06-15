9690ba755cdf82f8b408b0219bbfd431796ae32b
류인환 <mmzz0219@gmail.com>
2024-06-15 21:29:01 +0900

    feat : Qeurydsl 설정 추가

--- src/main/java/site/doto/global/config/AppConfig.java
+++ src/main/java/site/doto/global/config/AppConfig.java
@@ -1,6 +1,7 @@
 package site.doto.global.config;
 
 import com.p6spy.engine.spy.P6SpyOptions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -10,11 +11,16 @@ import site.doto.global.logtracer.LogTraceAspect;
 import site.doto.global.p6spy.P6spySqlFormatConfiguration;
 
 import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Configuration
 @RequiredArgsConstructor
 public class AppConfig {
     private final LogTrace logTrace;
+
+    @PersistenceContext
+    public EntityManager entityManager;
 
     @Bean
     @ConditionalOnProperty(value = "logTracer", havingValue = "true")
@@ -25,5 +31,10 @@ public class AppConfig {
     @PostConstruct
     public void setLogMessageFormat() {
         P6SpyOptions.getActiveInstance().setLogMessageFormat(P6spySqlFormatConfiguration.class.getName());
+    }
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
     }
 }
