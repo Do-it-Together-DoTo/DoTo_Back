9194182a909457f97f77062e3d907f1caa98f3ae
류인환 <mmzz0219@gmail.com>
2024-06-17 17:39:12 +0900

    feat : MemberBetting 복합키 설정

--- src/main/java/site/doto/domain/betting/dto/BettingJoinReq.java
+++ src/main/java/site/doto/domain/betting/dto/BettingJoinReq.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import site.doto.domain.betting.entity.Betting;
 import site.doto.domain.member.entity.Member;
 import site.doto.domain.member_betting.entity.MemberBetting;
+import site.doto.domain.member_betting.entity.MemberBettingPK;
 
 import javax.validation.constraints.NotNull;
 
@@ -17,6 +18,7 @@ public class BettingJoinReq {
 
     public MemberBetting toEntity(Member member, Betting betting) {
         return MemberBetting.builder()
+                .memberBettingPK(new MemberBettingPK(member.getId(), betting.getId()))
                 .member(member)
                 .betting(betting)
                 .cost(cost)
