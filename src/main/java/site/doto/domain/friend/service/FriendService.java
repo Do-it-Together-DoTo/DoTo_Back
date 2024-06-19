c5d1767b83efb3341d2f137e93fb6414c2409e24
eyxnz <todaytubuck@naver.com>
2024-06-19 13:49:10 +0900

    feat : ref 타입의 Member Id 값을 equals로 비교하도록 수정

--- src/main/java/site/doto/domain/friend/service/FriendService.java
+++ src/main/java/site/doto/domain/friend/service/FriendService.java
@@ -34,7 +34,7 @@ public class FriendService {
         Member fromMember = memberRepository.findById(friendRequestReq.getFromMemberId())
                 .orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
 
-        if(toMember.getId().compareTo(fromMember.getId()) == 0) {
+        if(toMember.getId().equals(fromMember.getId())) {
             throw new CustomException(FRIEND_SELF_REQUEST);
         }
 
