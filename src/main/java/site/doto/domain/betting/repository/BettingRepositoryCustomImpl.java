84bd7a42dca20e67f90036cf51bcdb86f54de051
Cutaku <107826896+Cutaku@users.noreply.github.com>
2024-06-19 17:21:59 +0900

    fix : 오픈 베팅 조회 시 이미 참여한 베팅은 조회되지 않도록 수정 (#110)

--- src/main/java/site/doto/domain/betting/repository/BettingRepositoryCustomImpl.java
+++ src/main/java/site/doto/domain/betting/repository/BettingRepositoryCustomImpl.java
@@ -14,6 +14,7 @@ import java.util.List;
 import static site.doto.domain.betting.entity.QBetting.betting;
 import static site.doto.domain.friend.entity.QFriend.friend;
 import static site.doto.domain.member.entity.QMember.member;
+import static site.doto.domain.member_betting.entity.QMemberBetting.memberBetting;
 
 @RequiredArgsConstructor
 public class BettingRepositoryCustomImpl implements BettingRepositoryCustom{
@@ -31,6 +32,11 @@ public class BettingRepositoryCustomImpl implements BettingRepositoryCustom{
                 .leftJoin(betting.member, member).fetchJoin()
                 .where(betting.todo.date.goe(LocalDate.now()))
                 .where(betting.member.in(subQuery))
+                .where(JPAExpressions
+                        .selectFrom(memberBetting)
+                        .where(memberBetting.betting.id.eq(betting.id))
+                        .where(memberBetting.member.id.eq(memberId))
+                        .notExists())
                 .fetch();
     }
 }
