f119bcdb9b35da7acd4d1d2d0a477eb9c2d39a57
eyxnz <todaytubuck@naver.com>
2024-06-27 18:49:33 +0900

    feat : 친구 목록 Dto 수정

--- src/main/java/site/doto/domain/relation/dto/RelationListReq.java
+++ src/main/java/site/doto/domain/relation/dto/RelationListReq.java
@@ -1,10 +1,12 @@
 package site.doto.domain.relation.dto;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
 
 @Data
 public class RelationListReq {
-    private Long lastFriendId;
-
-    private String lastFriendTodoDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSSSSS")
+    private LocalDateTime lastFriendLastUpload;
 }
