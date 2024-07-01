302bb3ebd49249e102abb51b7265f129018876b3
jeongjiyeon315 <jung_j_yeon@naver.com>
2024-07-01 18:18:41 +0900

    feat : Todo 또하기 기능 구현

--- src/main/java/site/doto/domain/todo/dto/TodoRedoReq.java
+++ src/main/java/site/doto/domain/todo/dto/TodoRedoReq.java
@@ -1,8 +1,12 @@
 package site.doto.domain.todo.dto;
 
 import lombok.Data;
+import site.doto.domain.category.entity.Category;
+import site.doto.domain.member.entity.Member;
+import site.doto.domain.todo.entity.Todo;
 
 import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 
 @Data
 public class TodoRedoReq {
@@ -11,4 +15,14 @@ public class TodoRedoReq {
 
     @NotNull
     private String date;
+
+    public Todo toEntity(Member member, Category category, Todo todo, LocalDate date) {
+        return Todo.builder()
+                .member(member)
+                .category(category)
+                .contents(todo.getContents())
+                .date(date)
+                .isDone(false)
+                .build();
+    }
 }
