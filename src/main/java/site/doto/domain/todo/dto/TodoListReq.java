640ef31a30039c0790d496ff994121d076bb00e6
jeongjiyeon315 <jung_j_yeon@naver.com>
2024-07-03 13:19:45 +0900

    feat : DateFormat 설정

--- src/main/java/site/doto/domain/todo/dto/TodoListReq.java
+++ src/main/java/site/doto/domain/todo/dto/TodoListReq.java
@@ -1,11 +1,14 @@
 package site.doto.domain.todo.dto;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 
 @Data
 public class TodoListReq {
     @NotNull
-    private String date;
+    @DateTimeFormat(pattern = "yyyyMMdd")
+    private LocalDate date;
 }
