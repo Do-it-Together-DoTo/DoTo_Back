b7887a92eefaa9ac410c1709adbcda13ef4b4ff4
Cutaku <mmzz0219@gmail.com>
2024-07-16 17:33:01 +0900

    feat : 참여 중인 채팅방 조회 API 구현

--- src/main/java/site/doto/domain/chatroom/dto/ChatRoomListRes.java
+++ src/main/java/site/doto/domain/chatroom/dto/ChatRoomListRes.java
@@ -1,10 +1,12 @@
 package site.doto.domain.chatroom.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class ChatRoomListRes {
     private List<ChatRoomDto> chatRooms;
 }
