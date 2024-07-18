9b053a89ed3d729ef3da2ab4065317fdf86c5031
eyxnz <todaytubuck@naver.com>
2024-07-18 16:47:02 +0900

    test : 회원 정보 수정 테스트 작성

--- src/test/java/site/doto/domain/member/controller/MemberControllerTest.java
+++ src/test/java/site/doto/domain/member/controller/MemberControllerTest.java
@@ -28,6 +28,7 @@ import static org.springframework.restdocs.request.RequestDocumentation.paramete
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static site.doto.global.status_code.ErrorCode.*;
 import static site.doto.global.status_code.SuccessCode.*;
 
 @Transactional
@@ -206,7 +207,7 @@ class MemberControllerTest {
     }
 
     @Test
-    @DisplayName("회원 정보 수정_성공")
+    @DisplayName("회원 정보 수정 - 성공")
     public void member_modify_success() throws Exception {
         //given
         MemberModifyReq memberModifyReq = new MemberModifyReq();
@@ -254,6 +255,78 @@ class MemberControllerTest {
                                 .responseSchema(Schema.schema("회원 정보 수정 Response"))
                                 .build())
                 ));
+    }
+
+    @Test
+    @DisplayName("회원 정보 수정 - 검증 실패")
+    public void member_modify_validation_fail() throws Exception {
+        // given
+        MemberModifyReq memberModifyReq = new MemberModifyReq();
+        memberModifyReq.setNickname("     ");
+        memberModifyReq.setDescription("닉네임이 공백이면 안 돼~");
+
+        String content = gson.toJson(memberModifyReq);
+
+        // when
+        ResultActions actions = mockMvc.perform(
+                patch("/members/modify")
+                        .header("Authorization", "Bearer " + jwtToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content)
+        );
+
+        // then
+        actions
+                .andExpect(jsonPath("$.header.httpStatusCode").value(BIND_EXCEPTION.getHttpStatusCode()))
+                .andExpect(jsonPath("$.header.message").value(BIND_EXCEPTION.getMessage()));
+    }
+
+    @Test
+    @DisplayName("회원 정보 수정 - 닉네임 중복")
+    public void member_modify_nickname_duplicated() throws Exception {
+        // given
+        MemberModifyReq memberModifyReq = new MemberModifyReq();
+        memberModifyReq.setNickname("test_user2");
+        memberModifyReq.setDescription("닉네임이 중복이면 안 돼~");
+
+        String content = gson.toJson(memberModifyReq);
+
+        // when
+        ResultActions actions = mockMvc.perform(
+                patch("/members/modify")
+                        .header("Authorization", "Bearer " + jwtToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content)
+        );
+
+        // then
+        actions
+                .andExpect(jsonPath("$.header.httpStatusCode").value(NICKNAME_DUPLICATED.getHttpStatusCode()))
+                .andExpect(jsonPath("$.header.message").value(NICKNAME_DUPLICATED.getMessage()));
+    }
+
+    @Test
+    @DisplayName("회원 정보 수정 - 한줄소개 길이 초과")
+    public void member_modify_description_length() throws Exception {
+        // given
+        MemberModifyReq memberModifyReq = new MemberModifyReq();
+        memberModifyReq.setNickname("test_user1");
+        memberModifyReq.setDescription("한줄소개는 20자까지.. 이거 은근 길다.. 어떻게 하면 20자를 넘길 수 있을지 고민을 해봐야 할 듯?");
+
+        String content = gson.toJson(memberModifyReq);
+
+        // when
+        ResultActions actions = mockMvc.perform(
+                patch("/members/modify")
+                        .header("Authorization", "Bearer " + jwtToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content)
+        );
+
+        // then
+        actions
+                .andExpect(jsonPath("$.header.httpStatusCode").value(BAD_REQUEST.getHttpStatusCode()))
+                .andExpect(jsonPath("$.header.message").value(BAD_REQUEST.getMessage()));
     }
 
     @Test
